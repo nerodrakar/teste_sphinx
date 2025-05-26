0e20d523deb59830df05bc33bd7e7313c6bb3ab1
Luiz Henrique <luizhenriquerezio@gmail.com>
2025-05-26 15:19:14 -0300

    feat: correção dos temas sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,8 +29,7 @@ language = 'pt'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_permalinks_icon = '<span>#</span>'
-html_theme = 'sphinxawesome_theme'
+html_theme = 'python_docs_theme'
 
 html_static_path = ['_static']
 
