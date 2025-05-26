fc7ae026374e1417b8a02d123a7c25bb3661643e
Luiz Henrique <luizhenriquerezio@gmail.com>
2025-05-26 15:24:06 -0300

    feat: correção do tema

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ language = 'pt'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'python_docs_theme'
+html_theme = 'furo'
 
 html_static_path = ['_static']
 
